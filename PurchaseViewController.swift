7f87d871e0aec9a1bbab22304bfc9c099a785858
Resdan Logan <rezn5447@gmail.com>
2017-03-29 14:44:01 -0700

    Update PurchaseViewController.swift

--- PurchaseViewController.swift
+++ PurchaseViewController.swift
@@ -122,12 +122,10 @@ class PurchaseViewController: UIViewController, UITableViewDataSource, UITableVi
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ConfirmSegue"{
             calculateTotal()
